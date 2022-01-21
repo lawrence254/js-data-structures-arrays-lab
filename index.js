18ec1a179bb09263f790cbb786e6cec987241fb4
lawrence254 <lawrenslava1@gmail.com>
2022-01-22 01:02:08 +0300

    afterEach test hook fails to reset the array back to the original prestine array even after setting it explicitly

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution here!
+
+// const prepend = ["Milo", "Otis", "Garfield"];
+// const removeLast = ["Milo", "Otis", "Garfield"];
+// const removeFirst = ["Milo", "Otis", "Garfield"];
+
+addPetsToEnd = (driver) => {
+    driver.push("Odie")
+    return driver;
+};
+
+function addPetsToBegining(driver) {
+    driver.unshift("Odie")
+    return driver;
+}
+
+removeLastPet = (driver) => {
+    driver.pop("Garfield")
+    return driver
+}
+
+removeFirstPet = (driver) => {
+    driver.shift("Milo")
+    return driver
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,10 +1,22 @@
 const sinon = require("sinon");
 
 describe("index.js", function () {
+  const driver = ["Milo", "Otis", "Garfield"];
+
+  resetDriver = () => {
+    driver = ["Milo", "Otis", "Garfield"];
+    return driver;
+  }
+
+  afterEach(function () {
+    expect(driver, 'Recreate original driver array').to.eql(resetDriver());
+  });
+
   describe("Array functions", function () {
     describe("append", function () {
       it('appends pet "Odie" to the end of the pets array', function () {
-        expect(append).to.have.ordered.members([
+        // addPetsToEnd()
+        expect(addPetsToEnd(driver)).to.have.ordered.members([
           "Milo",
           "Otis",
           "Garfield",
@@ -15,7 +27,8 @@ describe("index.js", function () {
 
     describe("prepend", function () {
       it("prepends a pet to the beginning of the pets array", function () {
-        expect(prepend).to.have.ordered.members([
+        // addPetsToBegining()
+        expect(addPetsToBegining(driver)).to.have.ordered.members([
           "Odie",
           "Milo",
           "Otis",
@@ -26,7 +39,8 @@ describe("index.js", function () {
 
     describe("removeLast", function () {
       it("removes the last pet from the pets array", function () {
-        expect(removeLast)
+        // removeLastPet()
+        expect(removeLastPet(driver))
           .to.have.ordered.members(["Milo", "Otis"])
           .and.to.not.include("Garfield");
       });
@@ -34,6 +48,7 @@ describe("index.js", function () {
 
     describe("removeFirst", function () {
       it("removes the First pet from the pets array", function () {
+        removeFirstPet()
         expect(removeFirst)
           .to.have.ordered.members(["Otis", "Garfield"])
           .and.to.not.include("Milo");
